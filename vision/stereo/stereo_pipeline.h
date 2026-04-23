bd23eb604f0a6bfd8e794eddf856ab2983c4cb0f
ramelard <rob.amelard@gmail.com>
2026-04-23 10:48:42 -0400

    Added helper comments

--- vision/stereo/stereo_pipeline.h
+++ vision/stereo/stereo_pipeline.h
@@ -2,10 +2,13 @@
 #include <opencv2/core.hpp>
 namespace vision
 {
+    // (Incomplete) minimal stereoscopic imaging pipeline with depth mapping.
     class StereoPipeline
     {
     public:
         StereoPipeline();
+
+        // Called on every stereo frame acquisition.
         cv::Mat ComputeDisparity(const cv::Mat &left, const cv::Mat &right);
 
     private:
